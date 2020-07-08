41aa39f02dedd7235ed4cde0386ba6121c4f6f0a
Benson <b4e0n0s3o1n@gmail.com>
2020-07-08 18:36:45 +0800

    Create rectangle object.

---
+++ utils/shape.py
@@ -0,0 +1,16 @@
+from PySide2.QtGui import *
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+
+class Shape(object):
+    def __init__(self):
+        self.firstPos = None
+        self.endPos = None
+
+    def paintRect(self, painter):
+        leftTop = self.firstPos
+        rightBottom = self.endPos
+        rectWidth = rightBottom.x() - leftTop.x()
+        rectHeight = rightBottom.y() - leftTop.y()
+        painter.setPen(QColor(0, 255, 0))
+        painter.drawRect(leftTop.x(), leftTop.y(), rectWidth, rectHeight)
