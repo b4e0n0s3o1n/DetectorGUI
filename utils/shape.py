f4615f48f46d7d0f3411af241d7c7baef5ee9a30
b4e0n0s3o1n <b4e0n0s3o1n@gmail.com>
2020-08-04 17:00:15 +0800

    Draw text at the top-left.

--- utils/shape.py
+++ utils/shape.py
@@ -6,6 +6,7 @@ class Shape(object):
     def __init__(self):
         self.firstPos = None
         self.endPos = None
+        self.digit = None
 
     def size(self):
         width = self.endPos.x() - self.firstPos.x()
@@ -13,6 +14,7 @@ class Shape(object):
         return (width, height)
 
     def paintRect(self, painter):
+        # Draw rectangle.
         leftTop = self.firstPos
         rightBottom = self.endPos
         rectWidth = rightBottom.x() - leftTop.x()
@@ -20,6 +22,17 @@ class Shape(object):
         painter.setPen(QColor(0, 255, 0))
         painter.drawRect(leftTop.x(), leftTop.y(), rectWidth, rectHeight)
 
+        # Draw text at the top-left
+        if self.digit is not None:
+            textX = self.firstPos.x()
+            textY = self.firstPos.y() - 2
+            if textY < 10:      # If position is too top.
+                textY += 10
+            font = QFont()
+            font.setPointSize(8)
+            painter.setFont(font)
+            painter.drawText(textX, textY, str(self.digit))
+            
     def isInShape(self, point):
         if (self.firstPos.x() <= point.x() <= self.endPos.x()) \
             and (self.firstPos.y() <= point.y() <= self.endPos.y()):
